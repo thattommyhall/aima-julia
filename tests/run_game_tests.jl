cf0f9b2cc5f4e590784297cae97360a04199ff8a
thattommyhall <thattommyhall@gmail.com>
2018-10-28 01:56:08 +0100

    Move @tests under a @testset so there is a summary printed

--- tests/run_game_tests.jl
+++ tests/run_game_tests.jl
@@ -6,14 +6,15 @@ using aimajulia;
 
 #The following game tests are from the aima-python doctest
 
-@test minimax_decision("A", Figure52Game()) == "A1";
-
-@test alphabeta_full_search("A", Figure52Game()) == "A1";
-
-@test alphabeta_search("A", Figure52Game()) == "A1";
-
-@test play_game(Figure52Game(), alphabeta_player, alphabeta_player) == 3;
-
+@testset "minimax" begin
+    @test minimax_decision("A", Figure52Game()) == "A1";
 end
 
+@testset "alphabeta" begin
+    @test alphabeta_full_search("A", Figure52Game()) == "A1";
+    @test alphabeta_search("A", Figure52Game()) == "A1";
+end
 
+@testset "play_game" begin
+    @test play_game(Figure52Game(), alphabeta_player, alphabeta_player) == 3;
+end
